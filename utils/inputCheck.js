a72a7e5ffe2ff263b290d07b5a18d66aab596b16
Synui <jonathon.schoenholz@yahoo.com>
2022-01-20 18:37:58 -0500

    Added voter routes

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -2,12 +2,7 @@
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
-  const errors = inputCheck(req.body, 'party_id');
-
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
+  const errors = [];
 
   props.forEach((prop) => {
     // if property is blank or doesn't exist, add to errors array
